a6224161f6c2e7ffe9d6afce3ae89b3406ad2f2c
pratik.dimble <pratik.dimble@nitorinfotech.com>
2024-12-09 15:09:28 +0530

    sort

---
+++ src/components/EmsSearch.vue
@@ -0,0 +1,19 @@
+<template>
+    
+</template>
+<script setup>
+const props = defineProps([
+    "empList",
+    "loading"
+]);
+
+const emit = defineEmits(['editClick', 'removeClick'])
+
+const editHandler = (id) => {
+    emit('editClick', id);
+}
+const deleteHandler = (id) => {
+    emit('removeClick', id);
+}
+</script>
+<style scoped></style>
